8567caf3e2b00726380064245b139817307f12bd
saiie5 <saimavuduru4@gmail.com>
2020-10-24 17:11:20 +0530

    js files

---
+++ practice.js
@@ -0,0 +1,108 @@
+var a =15;
+var b = 15;
+(a>b) ? console.log("b is smaller") : (a<b) ? console.log("a is smaller than b"): console.log("a equal b");
+
+
+
+
+a ="97004676"
+b="970046xZ"
+ 
+if(a===b){
+    console.log("coorect passcode")
+}
+else {
+    console.log("wrong passcode")
+}
+
+
+
+
+
+a= 15;
+if(a%2!=0){
+    console.log("it os odd")
+}
+else{
+    console.log("it is even")
+}
+
+
+
+
+
+
+
+a=24
+if(a<20 && a > 29){
+    console.log("twenties")
+}
+else{
+    console.log("not twenties")
+}
+
+
+
+
+
+
+
+ a = 20;
+ b =12; c=13;
+  if (a<b&&a<c&&b<c){
+      console.log(a)
+      console.log(b)
+      console.log(c)
+      
+  }
+  else if(b<a&&b<c&&c<a){
+      console.log(b)
+      console.log(c)
+      console.log(a)
+  }
+  else if (b<a) {
+      console.log(c)
+      console.log(b)
+      console.log(a)
+  }
+
+
+
+  a=30;
+  b=20;
+   (a>b) ? console.log("a is greater tha b"):("b is greater than a");
+
+
+
+
+   var day ="monday"
+
+
+   switch (day){
+       case "monday" :
+           console.log("mon")
+           break;
+           case "tuesday":
+               console.log("tue")
+               break;
+               case "wednesday":
+                   console.log("wed")
+                   break;
+                   case "thurseday":
+                       console.log("thur")
+                       break;
+                       case "friday":
+                           console.log("fri")
+                           break;
+                           case("saturday"):
+                           console.log("sat")
+                           break;
+                           case ("sunday"):
+                               console.log("Sun")
+                               break;
+   
+
+   }
+
+
+
